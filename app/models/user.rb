461913bf4404da87c92f00a73fdec2ce026dab7c
jatellezs <jatellez@miuandes.cl>
2019-05-15 09:29:42 -0400

    migrations devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   	has_many :user_tickets	
 	has_many :tickets, :through => :user_tickets
 
 	validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}, uniqueness: true
 
 	validates :phone, length: {minimum: 9, maximum: 12}, allow_blank: true
-  	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "assword must be between 8 to 12 alphanumeric characters"}
+  	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "Password must be between 8 to 12 alphanumeric characters"}
 
 
 
 end
+ 
